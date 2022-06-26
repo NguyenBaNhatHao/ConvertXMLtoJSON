5f9d00abe0849d7edba3871f4e770b9025a9d22a
sincollmm <56375360+sincollmm@users.noreply.github.com>
2022-06-26 21:01:18 +0700

    Add project files.

---
+++ ConvertXMLtoJSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Xml;
+
+XmlDocument doc = new XmlDocument();
+
+using (StreamReader streamReader = new StreamReader("C:\\Users\\Admin\\source\\repos\\ConvertXMLtoJSON\\ConvertXMLtoJSON\\XmlFile\\BangTyGiaVangSJC.xml", Encoding.UTF8))
+{
+    var contents = streamReader.ReadToEnd();
+    doc.LoadXml(contents);
+}
+XmlNode root = doc.DocumentElement;
+root.RemoveChild(root.FirstChild);
+string json = JsonConvert.SerializeXmlNode(root);
+Console.WriteLine(json);
